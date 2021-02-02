8d13d4191b2ea13e8f8c5285df1e0255cbb181aa
corrina <csivak345@gmail.com>
2021-02-02 00:05:00 -0500

    implemented most instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,23 +1,40 @@
+use std::fmt::Error;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
-use std::fmt::Error;
-use crate::isa::Instruction;
+use rand::{thread_rng, Rng};
 
-pub struct Memory([u8; 0xFFF]);
+use crate::isa::{Instruction, ISA};
+use rand::rngs::ThreadRng;
+use std::borrow::{Borrow, BorrowMut};
+
+const MEM_SIZE: usize = 0xFFF;
+const PROG_START: usize = 0x200;
+
+pub struct Memory([u8; MEM_SIZE]);
 
 impl Memory {
     pub fn get_instruction(&self, addr: usize) -> Instruction {
-        let offset = addr * 2;
-        Instruction::from([self.0[offset], self.0[offset+1]])
+        Instruction::from([self.0[addr], self.0[addr + 1]])
+    }
+    pub fn get_u16(&self, addr: usize) -> u16 {
+        u16::from_be_bytes([self.0[addr], self.0[addr + 1]])
+    }
+    pub fn set_u16(&mut self, addr: usize, val: u16) {
+        for (offset, byte) in val.to_be_bytes().iter().enumerate() {
+            self.0[addr + offset] = *byte;
+        }
     }
 }
 
 pub struct Program {
-    v: [i8; 16],
+    v: [i8; 0xF],
     dt: u8,
     st: u8,
     pc: u16,
+    stk: usize,
+    i: u16,
+    rng: ThreadRng,
     mem: Memory,
 }
 
@@ -25,12 +42,15 @@ impl Program {
     pub fn from(mut file: File) -> Self {
         let mut program = Self {
             v: Default::default(),
-            dt: Default::default(),
-            st: Default::default(),
-            pc: 0x200,
-            mem: Memory([0; 0xFFF]),
+            dt: 0,
+            st: 0,
+            pc: PROG_START as u16,
+            stk: 0x0,
+            i: 0,
+            rng: rand::thread_rng(),
+            mem: Memory([0; MEM_SIZE]),
         };
-        file.read(&mut program.mem.0[0x200..]).unwrap();
+        file.read(&mut program.mem.0[PROG_START..]).unwrap();
         program
     }
 
@@ -38,11 +58,82 @@ impl Program {
     pub fn run_program(&mut self) {
         loop {
             let inst = self.mem.get_instruction(self.pc as usize);
-
+            self.exec(inst);
         }
     }
 
     fn exec(&mut self, instruction: Instruction) {
-
+        let inc: bool = true;
+        match instruction.0 {
+            ISA::SYS { addr } => { self.pc = addr as u16 },
+            ISA::CLS => {}
+            ISA::RET => {
+                self.stk -= 2;
+                self.pc = self.mem.get_u16(self.stk);
+            }
+            ISA::JP { addr } => { self.pc = addr as u16 },
+            ISA::CALL { addr } => {
+                self.stk += 2;
+                self.mem.set_u16(self.stk, self.pc);
+                self.pc = addr as u16;
+            }
+            ISA::SEI { vx, imm } => {
+                if self.v[vx] == imm as i8 {
+                    self.pc += 2;
+                }
+            }
+            ISA::SNEI { vx, imm } => {
+                if self.v[vx] != imm as i8 {
+                    self.pc += 2;
+                }
+            }
+            ISA::SE { vx, vy } => {
+                if self.v[vx] == self.v[vy] {
+                    self.pc += 2;
+                }
+            }
+            ISA::LDI { vx, imm } => { self.v[vx] = imm as i8 }
+            ISA::ADDI { vx, imm } => { self.v[vx] += imm }
+            ISA::LD { vx, vy } => { self.v[vx] = self.v[vy] }
+            ISA::OR { vx, vy } => { self.v[vx] |= self.v[vy] }
+            ISA::AND { vx, vy } => { self.v[vx] &= self.v[vy] }
+            ISA::XOR { vx, vy } => { self.v[vx] ^= self.v[vy] }
+            ISA::ADD { vx, vy } => {
+                self.v[vx] += self.v[vy];
+                self.v[0xF] = i8::from(self.v[vx] as u8 > 255);
+            }
+            ISA::SUB { vx, vy } => {
+                self.v[0xF] = i8::from(self.v[vx] > self.v[vy]);
+                self.v[vx] -= self.v[vy];
+            }
+            ISA::SHR { vx, vy } => { unimplemented!() }
+            ISA::SUBN { vx, vy } => {
+                self.v[0xF] = i8::from(self.v[vx] < self.v[vy]);
+                self.v[vx] = self.v[vy] - self.v[vx];
+            }
+            ISA::SHL { .. } => { unimplemented!() }
+            ISA::SNE { vx, vy } => {
+                if self.v[vx] != self.v[vy] {
+                    self.pc += 2;
+                }
+            }
+            ISA::LDA { addr } => { self.i = addr as u16; }
+            ISA::JPO { addr } => { self.pc = (addr + self.v[0] as usize) as u16 }
+            ISA::RND { vx, imm } => {
+                self.v[vx] = self.rng.borrow_mut().gen_range((..255)) & imm;
+            }
+            ISA::DRW { .. } => {}
+            ISA::SKP { .. } => {}
+            ISA::SKNP { .. } => {}
+            ISA::LDDT { .. } => {}
+            ISA::LDKEY { .. } => {}
+            ISA::SETDT { .. } => {}
+            ISA::LDST { .. } => {}
+            ISA::ADDIR { .. } => {}
+            ISA::LDSPR { .. } => {}
+            ISA::LDBCD { .. } => {}
+            ISA::STR { .. } => {}
+            ISA::LDR { .. } => {}
+        };
     }
 }
--- src/isa.rs
+++ src/isa.rs
@@ -1,17 +1,16 @@
-use ux::{u12, u4};
-use crate::isa::ISA::{SYS, JP};
+use crate::isa::ISA::{JP, SYS};
 
-enum ISA {
+pub enum ISA {
     SYS { addr: usize },
     CLS,
     RET,
     JP { addr: usize },
     CALL { addr: usize },
-    SEI { vx: usize, imm: u8 },
-    SNEI { vx: usize, imm: u8 },
+    SEI { vx: usize, imm: i8 },
+    SNEI { vx: usize, imm: i8 },
     SE { vx: usize, vy: usize },
-    LDI { vx: usize, imm: u8 },
-    ADDI { vx: usize, imm: u8 },
+    LDI { vx: usize, imm: i8 },
+    ADDI { vx: usize, imm: i8 },
     LD { vx: usize, vy: usize },
     OR { vx: usize, vy: usize },
     AND { vx: usize, vy: usize },
@@ -24,19 +23,22 @@ enum ISA {
     SNE { vx: usize, vy: usize },
     LDA { addr: usize },
     JPO { addr: usize },
-    RND { vx: usize, imm: u8 },
+    RND { vx: usize, imm: i8 },
     DRW { vx: usize, vy: usize, size: usize },
     SKP { vx: usize },
     SKNP { vx: usize },
     LDDT { vx: usize },
+    LDKEY { vx: usize },
+    SETDT { vx: usize },
     LDST { vx: usize },
     ADDIR { vx: usize },
     LDSPR { vx: usize },
+    LDBCD { vx: usize },
     STR { vx: usize },
     LDR { vx: usize },
 }
 
-pub struct Instruction(ISA);
+pub struct Instruction(pub ISA);
 
 impl Instruction {
     pub fn from([high, low]: [u8; 2]) -> Self {
@@ -47,25 +49,78 @@ impl Instruction {
             return Instruction(ISA::RET);
         }
 
-        let opcode = high & 0b11110000;
-        return Instruction(match (opcode, low) {
-            (0x0, _) => ISA::SYS { addr: addr(raw) },
-            (0x1, _) => ISA::JP { addr: addr(raw) },
-            (0x2, _) => ISA::CALL {addr: addr(raw) }
+        let opcode = high & 0x0F;
+        let (addr, vx, vy, imm) = (
+            addr(raw),
+            vx(high),
+            vy(low),
+            imm(high),
+        );
+        return Instruction(match opcode {
+            0x0 => ISA::SYS { addr },
+            0x1 => ISA::JP { addr },
+            0x2 => ISA::CALL { addr },
+            0x3 => ISA::SEI { vx, imm },
+            0x4 => ISA::SNEI { vx, imm },
+            0x5 => ISA::SE { vx, vy },
+            0x6 => ISA::LDI { vx, imm },
+            0x7 => ISA::ADDI { vx, imm },
+            0x8 => match variant(low) {
+                0x0 => ISA::LD { vx, vy },
+                0x1 => ISA::OR { vx, vy },
+                0x2 => ISA::AND { vx, vy },
+                0x3 => ISA::XOR { vx, vy },
+                0x4 => ISA::ADD { vx, vy },
+                0x5 => ISA::SUB { vx, vy },
+                0x6 => ISA::SHR { vx, vy },
+                0x7 => ISA::SUBN { vx, vy },
+                0xE => ISA::SHL { vx, vy },
+                _ => panic!("help pls"),
+            },
+            0x9 => ISA::SNE { vx, vy },
+            0xA => ISA::LDA { addr },
+            0xB => ISA::JPO { addr },
+            0xC => ISA::RND { vx, imm },
+            0xD => ISA::DRW { vx, vy, size: u8::from_be(low) as usize },
+            0xE => match low {
+                0x9E => ISA::SKP { vx },
+                0xA1 => ISA::SKNP { vx },
+                _ => panic!("pls help!"),
+            },
+            0xF => match low {
+                0x07 => ISA::LDDT { vx },
+                0x0A => ISA::LDKEY { vx },
+                0x15 => ISA::SETDT { vx },
+                0x18 => ISA::LDST { vx },
+                0x1E => ISA::ADDIR { vx },
+                0x29 => ISA::LDSPR { vx },
+                0x33 => ISA::LDBCD { vx },
+                0x55 => ISA::STR { vx },
+                0x65 => ISA::LDR { vx },
+                _ => panic!("pls help!")
+            }
+
             _ => panic!("help pls")
         })
     }
 }
+
+const fn variant(low: u8) -> u8 {
+    u8::from_be((low & 0xF0) << 4)
+}
+
 const fn vx(high: u8) -> usize {
-    let x = (high & 0xF0FF) << 4;
+    let x = (high & 0xF0) << 4;
     u8::from_be(x) as usize
 }
+
 const fn vy(low: u8) -> usize {
     let y = (low & 0x0F);
     u8::from_be(y) as usize
 }
-const fn imm(low: u8) -> u8 {
-    u8::from_be(low)
+
+const fn imm(low: u8) -> i8 {
+    i8::from_be(low as i8)
 }
 const fn addr(raw: u16) -> usize {
     let addr = (raw & 0xF000) << 4;
